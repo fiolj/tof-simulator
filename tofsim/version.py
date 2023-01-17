d8a847a250516c83e6f800afe14232d0bf63f8a9
Juan Fiol <juanfiol@gmail.com>
2023-01-17 12:54:42 -0300

    fixed version file

--- tofsim/version.py
+++ tofsim/version.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-VERSION = '3.03'
-=======
 VERSION = '3.05'
->>>>>>> aa05e6542a8aeae3a0ce68984fa3ed9a52e556f0
+
 COPYRIGHT = """
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
