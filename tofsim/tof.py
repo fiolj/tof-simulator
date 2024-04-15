229d152a61456a9ab5ba642864b272517a414476
Juan Fiol <fiol@cab.cnea.gov.ar>
2024-04-15 10:22:04 -0300

    Fixed bug only occurring for extremely low acceleration field cases

--- tofsim/tof.py
+++ tofsim/tof.py
@@ -534,12 +534,11 @@ Construction parameters:
         #     v_s = v_0 + a_s * t_p
         #     Ts = coef * (self.s + 0.5 * a_s * t_p**2) / v_s
 
-
         a_d = self.Ed
         if a_d > 1.e-25:
             Td = coef / (a_d) * (-v_s + np.sqrt(np.square(v_s) + (2 * a_d * self.d)))
         else:
-            Td = coef * self.d / v_s
+            Td = coef *  v_s / (a_d**2 * self.d )
 
         v_d = v_s + a_d * Td
         TD = coef * self.D / v_d
